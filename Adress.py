6039797a7e62f4a12da34a840200e798cc72f1d8
PetrovEgor11 <dsk648400411@yandex.ru>
2025-04-24 13:40:07 +0300

    Update Adress.py

--- Adress.py
+++ Adress.py
@@ -5,3 +5,26 @@ class Address:
         self.street=street
         self.house=house
         self.apartament=apartment
+
+
+    def say_index (self):
+        print ("Индекс:", self.index)
+
+    def say_city (self):
+        print ("Город:", self.city)
+
+    def say_sreeet (self):
+        print ("Улица:", self.street)
+
+    def say_house (self):
+        print ("Дом:", self.house)
+    
+    def say_aapartament (self):
+        print ("Номер квартиры:", self.apartament)
+
+from_adrr = Address ()
+from_adrr.say_index()
+from_adrr.say_city()
+from_adrr.say_sreeet()
+from_adrr.say_house()
+from_adrr.say_aapartament()
