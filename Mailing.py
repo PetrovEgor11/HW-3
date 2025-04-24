441b7b968af2ad7ba62f7699c057e765cae07c9c
PetrovEgor11 <dsk648400411@yandex.ru>
2025-04-24 13:39:39 +0300

    Update Mailing.py

--- Mailing.py
+++ Mailing.py
@@ -4,3 +4,21 @@ class Mailing:
         self.from_address=from_address
         self.cost=cost
         self.track=track
+        
+    def say_to_address (self):
+        print ("Обращаться к", self.to_address)
+
+    def say_from_address (self):
+        print ("С адреса", self.from_address)
+
+    def say_cost (self):
+        print ("Стоимость", self.cost)
+
+    def say_track (self):
+        print ("Дом", self.track)
+
+Mail=Mailing ()
+Mail.say_to_address()
+Mail.say_from_address()
+Mail.say_cost()
+Mail.say_track()
