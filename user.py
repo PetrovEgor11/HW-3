71c338f877004a33da1e82aa9fa5cc2edaccf87a
PetrovEgor11 <dsk648400411@yandex.ru>
2025-05-06 11:18:15 +0300

    Описание изменений

--- user.py
+++ user.py
@@ -1,18 +1,20 @@
 class User:
-    def __init__ (self, first_name, last_name, full_name):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-        self.full_name = first_name +" "+ last_name
+        self.full_name = f"{first_name} {last_name}"
         
-    def sayFirstName (self):
-           print("Меня зовут:", self.first_name)
+    def say_first_name(self):
+        print("Меня зовут:", self.first_name)
                
-    def sayLastName(self):
-         print("Моя фамилия:", self.last_name)
-    def sayfull_name (self):
-         print("Мое полное имя:", self.full_name)
+    def say_last_name(self):
+        print("Моя фамилия:", self.last_name)
+         
+    def say_full_name(self):
+        print("Мое полное имя:", self.full_name)
 
-user = User ("Egor", "Petrov", "Egor Petrov")
-user.sayFirstName()
-user.sayLastName()
-user.sayfull_name()
+
+my_user = User("Ivan", "Petrov")
+my_user.say_first_name()
+my_user.say_last_name()
+my_user.say_full_name()
